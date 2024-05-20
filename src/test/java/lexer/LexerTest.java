156f853dd2ef3cf1c6224bdbd817781466c93d84
L0rable <finwalker@gmail.com>
2024-05-20 23:13:30 +1000

    Testing on Lexer getToken conditions

--- src/test/java/lexer/LexerTest.java
+++ src/test/java/lexer/LexerTest.java
@@ -1,6 +1,8 @@
 package lexer;
 
 import org.junit.jupiter.api.*;
+import token.Token;
+import token.TokenType;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -59,5 +61,137 @@ class LexerTest {
         char expectedCurChar = 'P';
         char actualCurChar = this.lexer.getCurrentChar();
         assertEquals(expectedCurChar, actualCurChar);
+    }
+
+    @Nested
+    class getTokenTests {
+        void checkTokensEqual(String actualSourceCode, Token expectedToken) {
+            lexer = new Lexer(actualSourceCode);
+            Token actualToken = lexer.getToken();
+            assertEquals(expectedToken.getTokenText(), actualToken.getTokenText());
+            assertEquals(expectedToken.getTokenKind(), actualToken.getTokenKind());
+        }
+
+        @Test
+        void getTokenEQTest() {
+            sourceCode = "=";
+            Token tokenEQ = new Token(sourceCode, TokenType.EQ);
+            checkTokensEqual(sourceCode, tokenEQ);
+        }
+
+        @Test
+        void getTokenEQEQTest() {
+            sourceCode = "==";
+            Token tokenEQEQ = new Token(sourceCode, TokenType.EQEQ);
+            checkTokensEqual(sourceCode, tokenEQEQ);
+        }
+
+        @Test
+        void getTokenPLUSTest() {
+            sourceCode = "+";
+            Token tokenPLUS = new Token(sourceCode, TokenType.PLUS);
+            checkTokensEqual(sourceCode, tokenPLUS);
+        }
+
+        @Test
+        void getTokenMINUSTest() {
+            sourceCode = "-";
+            Token tokenMINUS = new Token(sourceCode, TokenType.MINUS);
+            checkTokensEqual(sourceCode, tokenMINUS);
+        }
+
+        @Test
+        void getTokenASTERISKTest() {
+            sourceCode = "*";
+            Token tokenASTERISK = new Token(sourceCode, TokenType.ASTERISK);
+            checkTokensEqual(sourceCode, tokenASTERISK);
+        }
+
+        @Test
+        void getTokenSLASHTest() {
+            sourceCode = "/";
+            Token tokenSLASH = new Token(sourceCode, TokenType.SLASH);
+            checkTokensEqual(sourceCode, tokenSLASH);
+        }
+
+        @Test
+        void getTokenNOTEQTest() {
+            sourceCode = "!=";
+            Token tokenNOTEQ = new Token(sourceCode, TokenType.NOTEQ);
+            checkTokensEqual(sourceCode, tokenNOTEQ);
+        }
+
+        @Test
+        void getTokenLTTest() {
+            sourceCode = "<";
+            Token tokenLT = new Token(sourceCode, TokenType.LT);
+            checkTokensEqual(sourceCode, tokenLT);
+        }
+
+        @Test
+        void getTokenLTEQTest() {
+            sourceCode = "<=";
+            Token tokenLTEQ = new Token(sourceCode, TokenType.LTEQ);
+            checkTokensEqual(sourceCode, tokenLTEQ);
+        }
+
+        @Test
+        void getTokenGTTest() {
+            sourceCode = ">";
+            Token tokenGT = new Token(sourceCode, TokenType.GT);
+            checkTokensEqual(sourceCode, tokenGT);
+        }
+
+        @Test
+        void getTokenGTEQTest() {
+            sourceCode = ">=";
+            Token tokenGTEQ = new Token(sourceCode, TokenType.GTEQ);
+            checkTokensEqual(sourceCode, tokenGTEQ);
+        }
+
+        @Test
+        void getTokenSTRINGTest() {
+            sourceCode = "\"Hello World!\"";
+            String expected = "Hello World!";
+            Token tokenSTRING = new Token(expected, TokenType.STRING);
+            checkTokensEqual(sourceCode, tokenSTRING);
+        }
+
+        @Test
+        void getTokenNUMBERTest() {
+            sourceCode = "99";
+            Token tokenNUMBER = new Token(sourceCode, TokenType.NUMBER);
+            checkTokensEqual(sourceCode, tokenNUMBER);
+        }
+
+        @Test
+        void getTokenNUMBERTest1() {
+            sourceCode = "99.99";
+            Token tokenNUMBER = new Token(sourceCode, TokenType.NUMBER);
+            checkTokensEqual(sourceCode, tokenNUMBER);
+        }
+
+        @Test
+        void getTokenIDENTIFIERTest() {
+            sourceCode = "varName";
+            Token tokenIDENTIFIER = new Token(sourceCode, TokenType.IDENTIFIER);
+            checkTokensEqual(sourceCode, tokenIDENTIFIER);
+        }
+
+        @Test
+        void getTokenNEWLINETest() {
+            sourceCode = "\n";
+            String expected = "\\n";
+            Token tokenNEWLINE = new Token(expected, TokenType.NEWLINE);
+            checkTokensEqual(sourceCode, tokenNEWLINE);
+        }
+
+        @Test
+        void getTokenEOFTest() {
+            sourceCode = "\0";
+            String expected = "";
+            Token tokenEOF = new Token(expected, TokenType.EOF);
+            checkTokensEqual(sourceCode, tokenEOF);
+        }
     }
 }
